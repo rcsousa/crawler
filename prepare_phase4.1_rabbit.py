cb809ab908c126e3bda22d78f0158a203bb5cb5d
Ricardo Coelho de Sousa <ricardo.c.sousa@gmail.com>
2017-04-15 20:39:28 +0000

    code fix commit

---
+++ prepare_phase4.1_rabbit.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+from cloudant.client import Cloudant
+import requests, re, redis, os, pika, traceback, time
+
+RABBIT = str(os.getenv('RABBIT'))
+
+
+CLOUDANT_USERNAME = str(os.getenv('CLOUDANT_USERNAME'))
+CLOUDANT_PASSWORD = str(os.getenv('CLOUDANT_PASSWORD'))
+CLOUDANT_ACCCOUNT = str(os.getenv('CLOUDANT_ACCCOUNT'))
+CLOUDANT_DB = str(os.getenv('CLOUDANT_DB'))
+
+client = Cloudant(CLOUDANT_USERNAME, CLOUDANT_PASSWORD, account=CLOUDANT_ACCCOUNT, connect=True)
+
+
+try:
+        params = pika.URLParameters(RABBIT)
+	db = client[CLOUDANT_DB]
+        #params.socket_timeout = 5
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+	channel.basic_qos(prefetch_count=1)
+        queue_state = channel.queue_declare("jusbrasil_phase3", durable=True, passive=True)
+        queue_size = queue_state.method.message_count
+        payload = []
+        for  method, properties, body in channel.consume("jusbrasil_phase3"):
+		print body
+		headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    		PageFile = requests.get("http://www.jusbrasil.com.br"+body, headers=headers)
+    		soup = BeautifulSoup(PageFile.content, "html.parser")
+		payload = []
+    		for links in soup.find("article", { "class" : "text" }).findAll("p"):
+                	payload.append(links.text)
+			data = { 
+				'_id' : body,
+				'doc': payload 
+				}
+			my_document = db.create_document(data)
+		time.sleep(2)
+		#else:
+		#	 my_document = None
+		#if my_document.exists():
+                channel.basic_ack(method.delivery_tag)
+except:
+        print traceback.format_exc()
